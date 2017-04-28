ad1635add7eae764180a2d16a4c169e6a5b0434d
cat <catb0t@protonmail.ch>
2017-04-28 12:55:09 -0400

    improve API at the cost of memory
    sizeof bignum_t on x64: 8 * 5 = 40, 40 + 8 = 48

--- bignum.h
+++ bignum.h
@@ -1,14 +1,18 @@
-#line 1 "bignum"
-#include <stdint.h>
-
 #ifndef __BIGNUM_LCBNL_H
+  #line 2 "bignum"
+  #include <stdint.h>
+
   #define __BIGNUM_LCBNL_H
 
 typedef long double ldbl_t;
 
 typedef struct st_bignum_t { 
-  // UNUSED: int8_t sign_flag; // redundant? -1 = negative, 1 = positive, other vals invalid
-  
+  int8_t
+     * sign_flag, // ease-of-access pointers to data + 1
+     * int_dgts,  // = data + 2
+     * frc_dgts,  // = data + 3
+     * end_sen,   // = data + 4 + *int_dgts + *frc_dgts 
+
   /* 
     structure:
       SF   sign flag, like above, -1 for negative 1 for positive
@@ -17,13 +21,16 @@ typedef struct st_bignum_t {
       SC.. any number of significands'
       ES   ending sentinel (usually 10, any multi-digit number)
                SF ID FD SC SC SC SC SC SC SC SC SC SC SC SC ES
-    example: { -1  3  2  5  1  2  7  6  6  6  6  6  6  6  5 13 } --> -512.766666665
+    example: { -1  3  9  5  1  2  7  6  6  6  6  6  6  6  5 13 } --> -512.766666665
   */
-  int8_t* data;
+    * data;
   
   size_t uid;
 } bignum_t;
   
-char*     bignum_see (const bignum_t* const bn);
-bignum_t* bignum_new (const float fl, const double db, const ldbl_t ldb);
+char*      bignum_see (const bignum_t* const bn);
+bignum_t* bignum_ctor (const float fl, const double db, const ldbl_t ldb);
+bignum_t* bignum_dtor (bignum_t* const bn);
+ldbl_t bignum_to_ldbl (const bignum_t* const bn);
+
 #endif
